759975f76dfa972ada90473b466b00b55659851e
Simon-Oliver <stauffer.simon@outlook.com>
2019-07-09 20:57:31 +1000

    playing around with jwt

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import socketIOClient from 'socket.io-client';
+import io from 'socket.io-client';
 import Login from './Login';
-import Message from './Message';
-import Messages from './Messages';
 
 export default class Chat extends Component {
   state = {
@@ -13,41 +11,51 @@ export default class Chat extends Component {
     users: [],
     loggedIn: false,
     message: '',
-    messages: []
+    messages: [],
+    error: ''
   };
 
-  socket = socketIOClient(this.state.endpoint);
+  socket = io.connect(this.state.endpoint, { query: { token: sessionStorage.token } });
 
   componentDidMount() {
+    console.log(sessionStorage);
     // Very simply connect to the socket
-
     // Listen for data on the "outgoing data" namespace and supply a callback for what to do when we get one. In this case, we set a state variable
-    this.socket.emit('init_data');
-    this.socket.on('get_data', data =>
-      this.setState({ users: data.users, messages: data.messages })
-    );
-    this.socket.on('outgoing data', data => this.setState({ response: data.num }));
-    this.socket.on('outgoing users', users => this.setState({ users }));
-    this.socket.on('initUser', user => {
-      console.log('Init User Fired', user);
-      this.setState({ userName: user.userName, userId: user.userId });
-    });
-
-    this.socket.on('outgoing messages', message => {
-      console.log(message);
-      this.setState(prevState => ({ messages: [...prevState.messages, message] }));
-    });
-
-    this.socket.on('existing user', user =>
-      this.setState({ userName: user.userName, userId: user.userId })
-    );
-
-    // const watchID = navigator.geolocation.watchPosition(position => {
-    //   this.setState({ lat: position.coords.latitude, long: position.coords.longitude });
-    //   console.log(position.coords.latitude, position.coords.longitude);
+    // this.socket.emit('init_data');
+    // this.socket.on('get_data', data =>
+    //   this.setState({ users: data.users, messages: data.messages })
+    // );
+    // this.socket.on('outgoing data', data => this.setState({ response: data.num }));
+    // this.socket.on('outgoing users', users => this.setState({ users }));
+    // this.socket.on('initUser', user => {
+    //   console.log('Init User Fired', user);
+    //   this.setState({ userName: user.userName });
+    //   sessionStorage.token = user.token;
+    //   console.log(sessionStorage);
     // });
+    // this.socket.on('error', e => this.setState({ error: e }));
 
-    // this.setState({ geoLoc: watchID });
+    if (sessionStorage.token) {
+      console.log('componenstDidMount with sesssionstorage');
+      this.socket.emit('auth_check');
+    }
+    this.socket.on('create_user', data => {
+      if (data.error) {
+        this.setState({ error: data.error });
+      }
+      sessionStorage.token = data.token;
+      sessionStorage.userName = data.userName;
+      this.socket.emit('auth_check');
+      this.setState({ error: '' });
+    });
+    this.socket.on('auth', e => console.log(e));
+    // this.socket.on('outgoing messages', message => {
+    //   console.log(message);
+    //   this.setState(prevState => ({ messages: [...prevState.messages, message] }));
+    // });
+    // this.socket.on('existing user', user =>
+    //   this.setState({ userName: user.userName, userId: user.userId })
+    // );
   }
 
   componentWillUnmount() {
@@ -56,8 +64,7 @@ export default class Chat extends Component {
   }
 
   sendEmit = userName => {
-    this.socket.emit('newUser', userName);
-    this.setState({ loggedIn: true });
+    this.socket.emit('create_user', userName);
   };
 
   sendMessage = message => {
@@ -72,20 +79,17 @@ export default class Chat extends Component {
   };
 
   render() {
+    const isLoggedIn = this.state.error.type !== 'authentication_error';
+
     return (
       <div>
+        {isLoggedIn ? <p>Authenticated</p> : <p>Not Authenticated</p>}
         <Login
           loggedIn={this.state.loggedIn}
           sendEmit={this.sendEmit}
           users={this.state.users}
           userName={this.state.userName}
         />
-        {this.state.messages.length ? (
-          <Messages messages={this.state.messages} />
-        ) : (
-          <p>Loading...</p>
-        )}
-        <Message sendMessage={this.sendMessage} />
       </div>
     );
   }
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const uuid = require('uuid');
+const jwt = require('jsonwebtoken');
 
 // Port from environment variable or default - 4001
 const port = process.env.PORT || 5000;
@@ -11,45 +12,82 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const users = [];
+const users = [
+  {
+    userName: 'oli',
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6InBhdHJpY2siLCJpYXQiOjE1NjI2NTUwMzN9.fJnnWHJl65gLYS3--HM5L5Z-Ip5JQIDQT4PeHR88ujw'
+  }
+];
 const messages = [];
 const client = [];
+const secret = 'You can only see me if you are logged in.';
 
 // Setting up a socket with the namespace "connection" for new sockets
+
 io.on('connection', socket => {
-  console.log(socket.id);
-  socket.on('init_data', () => {
-    io.emit('get_data', { users, messages });
-  });
-
-  // Here we listen on a new namespace called "incoming data"
-  socket.on('incoming data', data => {
-    // Here we broadcast it out to all other sockets EXCLUDING the socket which sent us the data
-    socket.broadcast.emit('outgoing data', { num: data });
-  });
-
-  socket.on('newUser', userName => {
+  socket.on('auth_check', userName => {
     const existingUser = users.find(u => u.userName === userName);
-    if (existingUser) {
-      socket.emit('existing user', existingUser);
-    } else if (!existingUser) {
-      console.log('New client connected ' + socket.id);
-      const userObj = { userName, userId: socket.id };
-      console.log(userObj);
-      users.push(userObj);
-      socket.emit('initUser', userObj);
-      console.log('SOCKET_ID: ', userObj.userId);
-      io.emit('outgoing users', users);
-
-      console.log(users);
+    if (
+      socket.handshake.query &&
+      socket.handshake.query.token &&
+      socket.handshake.query.token !== 'undefined'
+    ) {
+      jwt.verify(socket.handshake.query.token, 'secret', function(err, decoded) {
+        if (err) {
+          io.emit('auth', { error: 'authentication_error' });
+        } else {
+          socket.decoded = decoded;
+          io.emit('auth', secret);
+        }
+      });
+    } else {
+      io.emit('auth', { error: 'authentication_error' });
     }
   });
 
-  socket.on('message', message => {
-    console.log(message);
-    messages.push(message);
-    io.emit('outgoing messages', message);
+  socket.on('create_user', userName => {
+    const existingUser = users.find(u => u.userName === userName);
+    if (existingUser) {
+      io.emit('create_user', { error: 'user_taken' });
+    } else {
+      const token = jwt.sign({ userName }, 'secret');
+      const userObj = { userName, token };
+      users.push(userObj);
+      socket.emit('create_user', userObj);
+    }
   });
+
+  // socket.on('init_data', () => {
+  //   io.emit('get_data', { users, messages });
+  // });
+
+  // // Here we listen on a new namespace called "incoming data"
+  // socket.on('incoming data', data => {
+  //   // Here we broadcast it out to all other sockets EXCLUDING the socket which sent us the data
+  //   socket.broadcast.emit('outgoing data', { num: data });
+  // });
+
+  // socket.on('newUser', userName => {
+  //   const existingUser = users.find(u => u.userName === userName);
+
+  //   if (existingUser) {
+  //     socket.emit('existing user', existingUser);
+  //   } else if (!existingUser) {
+  //     const token = jwt.sign({ userName }, 'secret');
+  //     const userObj = { userName, token };
+  //     users.push(userObj);
+  //     socket.emit('initUser', userObj);
+  //     io.emit('outgoing users', users);
+  //     console.log(users);
+  //   }
+  // });
+
+  // socket.on('message', message => {
+  //   console.log(message);
+  //   messages.push(message);
+  //   io.emit('outgoing messages', message);
+  // });
 
   // A special namespace "disconnect" for when a client disconnects
   socket.on('disconnect', () => console.log('Client disconnected ' + socket.id));
@@ -57,15 +95,17 @@ io.on('connection', socket => {
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
 
-// const bcrypt = require('bcryptjs');
+// const jwt = require('jsonwebtoken');
 
 // const myFunction = async () => {
-//   const pw = 'H123456';
-//   const hashedPw = await bcrypt.hash(pw, 8);
-//   console.log(hashedPw);
+//   // const token = jwt.sign({ _id: 'abc123' }, 'this is my secret', { expiresIn: '7days' });
+//   // console.log(token);
 
-//   const isMatch = await bcrypt.compare('123456', hashedPw);
-//   console.log(isMatch);
+//   const isTrue = jwt.verify(
+//     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6InBhdHJpY2siLCJpYXQiOjE1NjI2NTUwMzN9.fJnnWHJl65gLYS3--HM5L5Z-Ip5JQIDQT4PeHR88ujw',
+//     'secret'
+//   );
+//   console.log(isTrue);
 // };
 
 // myFunction();
